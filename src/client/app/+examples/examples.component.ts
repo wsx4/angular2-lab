a578dd9c710cde12a81e9e2e895f09309c18ffe3
arriya <wsx5@i.ua>
2016-05-24 20:46:05 +0300

    initial commit

---
+++ src/client/app/+examples/examples.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'sd-examples',
+    templateUrl: 'examples.component.html',
+    styleUrls: ['examples.component.css'],
+})
+export class ExamplesComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
