1aa1f295517c487432a1ba6d3d30db1f6307f56b
Sergey Prokopenko <s.prokopenko@synergetica.net>
2016-06-06 18:35:21 +0300

    form start

---
+++ src/client/app/+examples/simple-form/form1.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/common';
+
+import { Hero } from './hero';
+
+@Component({
+    moduleId: module.id,
+    selector: 'sd-form1',
+    templateUrl: 'form1.component.html'
+})
+export class Form1Component {
+    constructor() { }
+}
